e5710ce008f1e8f81c34bc4e0ca537a26c30bc75
RoloPineda <rolandopineda0@icloud.com>
2022-06-02 23:01:44 +0400

    Finished v1 of API

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+data = [{'likes': 13131, 'name': 'Jen', 'views':1000}, {'likes': 10, 'name': 'tim', 'views':1000},
+        {'likes': 10213, 'name': 'My API', 'views':20000},
+        {'likes': 10, 'name': 'Last vid', 'views':1000}]
+"""
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i),data[i])
+    print(response.json())
+
+input()
+response = requests.get(BASE + "video/12")
+print(response.json())
+"""
+
+response = requests.patch(BASE + "video/2", {"views":123, "likes": 200})
+print(response.json())
